42b2b29a5cb9873a8fa8f95ed3a5409cf172abad
Kunal Jindal <kunaljindal@Goku.local>
2026-09-07 08:22:29 +0530

    JPA annotations

--- BookMyShow/src/main/java/com/scaler/BookMyShow/models/BaseEntity.java
+++ BookMyShow/src/main/java/com/scaler/BookMyShow/models/BaseEntity.java
@@ -1,16 +1,28 @@
 package com.scaler.BookMyShow.models;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.util.Date;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 1,2,3....
     private int id;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
     private Date createdAt;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
     private Date updatedAt;
 
 }
--- BookMyShow/src/main/java/com/scaler/BookMyShow/models/Movie.java
+++ BookMyShow/src/main/java/com/scaler/BookMyShow/models/Movie.java
@@ -1,6 +1,8 @@
 package com.scaler.BookMyShow.models;
 
 import com.scaler.BookMyShow.models.enums.Feature;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +10,8 @@ import java.util.List;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "movies")
 public class Movie extends BaseEntity{
 
     private String title;
